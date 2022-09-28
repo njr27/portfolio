a0c7c938318a5ce6ddddde7bd6318dbb691d9932
njr27 <nivaldojunior00@gmail.com>
2022-09-28 16:52:41 -0300

    Site responsivado

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+const skills = document.querySelectorAll('.skills-content__card-content--item')
+const experiences = document.querySelectorAll('.experience-content__card-content--item')
+const largeImage = document.querySelector('.portfolio__cards--img')
+const smallImages = document.querySelectorAll('.portfolio__cards--wrapper-2')
+const footerLeft = document.querySelector('.footer__left')
+const footerRight = document.querySelector('.footer__right')
+const screenWidth = innerWidth
+
+screenWidth <= 540 ? console.log("mobile") : console.log("desktop")
+
+
+if(screenWidth <= 540){   
+    for (let skill of skills){
+        skill.classList.add('grid-12')
+    }
+
+    for (let experience of experiences){
+        experience.classList.add('grid-12')
+    }
+    
+    for(let smallImage of smallImages){
+        smallImage.classList.add('grid-6')
+    }
+    
+    largeImage.classList.add('grid-12')
+    footerLeft.classList.add('grid-12')
+    footerRight.classList.add('grid-12')
+
+
+}
